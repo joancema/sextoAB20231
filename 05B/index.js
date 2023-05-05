27b0df013202da793ff7eaaae6042cbc6a1b6c85
John Cevallos <joancemac@gmail.com>
2023-05-04 19:18:17 -0500

    REST

--- 05B/index.js
+++ 05B/index.js
@@ -7,21 +7,27 @@ const PUERTO = 2500;
 app.use(cors()).use(express.json())
 app.use('/public', express.static(__dirname+'/public'))
 
-const fruits = []
+let fruits = []
 
 // REST
 // get   -> Consultar
 app.get('/',(req,res)=>{
     res.status(200).json(fruits)
 })
-
 // get individual -> Consulta individual
-
-
-
-
+app.get('/:codigo',(req,res)=>{
+    // const codigo = req.params.codigo
+    const { codigo  } = req.params;
+    const fruitsSelect = fruits.filter(p=> p.codigo === codigo)
+    if (fruitsSelect.length>0)
+    {
+        return res.status(200).send(fruitsSelect[0])
+    }
+    res.status(404).send({
+        message:'La fruta con ese código no existe'
+    })
+})
 // post    -> Insertar
-
 app.post('/',(req, res )=>{
     // const body =  req.body
     const { body } =  req;
@@ -32,14 +38,26 @@ app.post('/',(req, res )=>{
     })
 
 })
-
 // put or patch   -> Actualizar
-
+app.put('/', (req,res)=>{
+    const { codigo, nombre, color, tipo } =  req.body
+    const fruit =  fruits.filter(p=> p.codigo===codigo)[0] || {}
+    fruit.nombre= nombre;
+    fruit.color= color;
+    fruit.tipo= tipo;
+    res.status(200).send({
+        message:'Fruta modificada con éxito',
+        response: fruit
+    })
+})
 // delete   -> Eliminar
-
-
-
-
+app.delete('/:codigo',(req, res)=>{
+    const { codigo  } = req.params;
+    fruits =  fruits.filter(p=> p.codigo !== codigo)
+    res.status(200).send({
+        message:`Fruta con código ${codigo} fue eliminada`
+    })
+})
 
 app.listen(PUERTO, ()=>{
     console.log(`SErver running http://localhost:${PUERTO}`);
