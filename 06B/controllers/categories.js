42beac03dae0a85a9f43f5e4d98d4b164d72d1ae
John Cevallos <joancemac@gmail.com>
2023-05-30 19:32:15 -0500

    cambios

---
+++ 06B/controllers/categories.js
@@ -0,0 +1,59 @@
+const { response} =  require('express')
+
+const { Category } = require('../models');
+
+//get
+// localhost:3000/api/categorias?limite=10,desde= 0
+// localhost:3000/api/categorias
+const getCategories = async  ( req, res =response  )=>{
+    const {  limite=100, desde=0   } =  req.query;
+    const query = {status: true}
+    const [categories, total ] = await   Promise.all(
+        Category
+        .find(query)
+        .skip(desde)
+        .limit(limite),
+        Category.count()
+    )
+    res.json({
+        categories,
+        total
+    })
+}
+//get
+//localhost:3000/api/categorias/2
+const getCategory = async (req, res =  response )=>{
+    const { id } =  req.params;
+    const category   =  await  Category.findById(id)
+    res.json(category);
+}
+//post
+const createCategory = async (req, res = response)=>{
+    //obtener el body
+    const {   status,  ...body } =  req.body;
+    
+    //validar
+    const existCategory = await Category.findOne({name:body.name});
+    if (existCategory)
+    {
+        return res.status(400).json({
+            message:`La categoria ${ body.name }  ya existe`
+        })
+    }
+    //armar el objeto para almacenar
+
+    const data= {
+        ...body
+    }
+    const category =  new Category(data);
+    //almacenar
+    const categoryNew =  await category.save();
+    //enviar respuesta
+    res.status(201).json(categoryNew)
+}
+
+
+//put
+
+
+//delete
